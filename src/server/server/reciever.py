0f738e9c69c37dc6f217297d1d51dbe52f885c23
aprit1997 <solankiarpit1997@gmail.com>
2017-11-05 10:45:07 +0530

    fix: added 0.5s delay for multicast response

--- src/server/server/reciever.py
+++ src/server/server/reciever.py
@@ -1,6 +1,7 @@
 import socket
 import struct
 import requests
+import time
 
 MCAST_GRP = '224.1.1.1'
 MCAST_PORT = 9000
@@ -19,5 +20,11 @@ sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 while True:
   data = sock.recv(10240)
   print(data)
-  print("http://"+data.decode('utf-8')+":8056/"+"?ip="+str(ip))
-  r = requests.get("http://"+data.decode('utf-8')+":8056/"+"?ip="+str(ip))
+  c = "http://"+data.decode('utf-8')+":8056/"+"?ip="+str(ip)
+  print(type(c))
+  d = "http://"+ data.decode('utf-8') + ":8056"
+  print(type(d))
+  time.sleep(0.5)
+  # r = requests.get(d)
+  r = requests.get(c)
+  print(r.status_code)
